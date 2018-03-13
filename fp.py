9506896c57e574b5ba142002c0996ca405a95218
pedrotp <pedro@pedrotp.com>
2018-03-13 12:25:20 -0400

    added an implementation of cross-checking that is not performing well

--- fp.py
+++ fp.py
@@ -39,6 +39,59 @@ def swap(t, a, b):
     n += t[b+1:]
     return n;
 
+# this function checks whether two edges are crossing each other before performing a swap
+# def crossing(t, i, j):
+#     a = t[i] # first point in first edge
+#     b = t[i + 1] # second point in first edge
+#     c = t[j - 1] # first point in second edge
+#     d = t[j] # second point in second edge
+#
+#     # 1) Find the small rectangle in which a crossig could occur.
+#     # 1.a) Find the left side of the rectangle.
+#     left = max(min(a[1], b[1]), min(c[1], d[1]))
+#     # 1.b-d) Find the other sides.
+#     right = min(max(a[1], b[1]), max(c[1], d[1]))
+#     top = min(max(a[2], b[2]), min(c[2], d[2]))
+#     bottom = max(min(a[2], b[2]), min(c[2], d[2]))
+#
+#     # 2) Find the intersection of the two lines, if it exists:
+#     # 2.a) Find the standard equation of the line passing through the two
+#     # points of each edge.
+#     # 2.a.i) Find the slope of the first line using m = (y2-y1)/(x2-x1)
+#     if a[1]-b[1] == 0:
+#         return True
+#     m1 = (a[2]-b[2])/(a[1]-b[1])
+#     # 2.a.ii) Find the standard equation of the line, ax + by = c and store
+#     # the coefficients a, b, c.  This requires some algebra wrangling to
+#     # understand where everything comes from.
+#     a1, b1, c1 = m1, 1, a[2]-m1*a[1]
+#     # 2.a.iii) Do it again for the second line.
+#     if c[1]-d[1] == 0:
+#         return True
+#     m2 = (c[2]-d[2])/(c[1]-d[1])
+#     a2, b2, c2 = m2, 1, c[2]-m2*c[1]
+#
+#     # 3) Solve using numpy.
+#     # 3.a) Form the matrices representing the coefficient matrix and
+#     # constant vector.
+#     A = np.matrix([[a1,b1], [a2,b2]])
+#     c = np.matrix([[c1], [c2]])
+#     # 3.b) Compute the inverse of A.  If it doesn't exist, the lines are
+#     # parallel and don't cross.
+#     try:
+#         Ai = np.linalg.inv(A)
+#         # 3.c) Compute the intersection of the lines.
+#         sol = Ai.dot(c)
+#
+#         # 4) Check that the solution lies inside of the rectangle from part 1.
+#         if (left <= sol[0] <= right) and (bottom <= sol[0] <= top):
+#             return True
+#         return False
+#     except:
+#         # Numpy's inverse function raises an exception if the inverse doesn't
+#         # exist, in which case the lines are parallel and don't cross.
+#         return False
+
 # this function contains the 2-opt algorithm that optimizes our tour
 def twoOpt(t):
     n = [] # a new temp tour
